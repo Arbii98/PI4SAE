968e972eed476250d9415012d0ccfe35aac43c73
Arbii98 <arbisaidi8@gmail.com>
2021-02-28 16:48:34 +0100

    First Commit: Projet Vide

---
+++ src/main/java/aces/esprit/Pi4SaeApplication.java
@@ -0,0 +1,17 @@
+package aces.esprit;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Pi4SaeApplication {
+	private static final Logger l = Logger.getLogger(Pi4SaeApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi4SaeApplication.class, args);
+		l.info("Bonjour chabeb");
+	}
+
+}
---
+++ src/test/java/aces/esprit/Pi4SaeApplicationTests.java
@@ -0,0 +1,13 @@
+package aces.esprit;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi4SaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
