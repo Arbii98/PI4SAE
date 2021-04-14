225c0d03ff392b15100984ac4ec15cb4ea86a503
wided Larayedh <wided.larayedh@gmail.com>
2021-04-15 00:56:22 +0100

    Entities done

--- DotNet/ConsommiTounsi.Domaine/Entities/Wided/Category.cs
+++ DotNet/ConsommiTounsi.Domaine/Entities/Wided/Category.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsommiTounsi.Domaine.Entities.Wided;
 
-namespace ConsommiTounsi.Domaine.Entities.Product
+namespace ConsommiTounsi.Domaine.Entities.Wided
 {
     public class Category
     {
         public int CategoryId { get; set; }
         public string nameCategory { get; set; }
         public string descriptionCategory { get; set; }
-
-
+        public virtual ICollection<Product> products { get; set; }
     }
 }
--- DotNet/ConsommiTounsi.Web/Controllers/Wided_Controller/CategoryController.cs
+++ DotNet/ConsommiTounsi.Web/Controllers/Wided_Controller/CategoryController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
-using ConsommiTounsi.Domaine.Entities.Product;
+using ConsommiTounsi.Domaine.Entities.Wided;
 
 namespace ConsommiTounsi.Web.Controllers.Product_Controller
 {
