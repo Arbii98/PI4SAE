370e559004aa17abdbaa3477c7eddd67bc2414b1
islem1920 <61006579+islem1920@users.noreply.github.com>
2021-04-22 17:54:49 +0100

    aff-pub

--- Spring/src/main/java/aces/esprit/controller/CommentController.java
+++ Spring/src/main/java/aces/esprit/controller/CommentController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import aces.esprit.entity.Comment;
+import aces.esprit.entity.CommentPk;
 import aces.esprit.service.CommentService;
 
 @RestController
@@ -52,6 +54,10 @@ public class CommentController {
 	@GetMapping(value = "/getall")
 	public List<Comment> getAllComment() {
 		return commentService.getAllComment();
+	}
+	@DeleteMapping()
+	public void deletePub(@RequestBody CommentPk commentpk) {
+		commentService.deleteById(commentpk);
 	}
 
 }
